883ed60b0935fa87b4ecb47d2d27a62265cd2867
Brad - Duncan <admin@foundryserver.com>
2025-12-03 12:31:28 -0800

    feat: Add module version string to module structure

--- mod_auth_mongodb.c
+++ mod_auth_mongodb.c
@@ -1240,5 +1240,6 @@ module auth_mongodb_module = {
     NULL,                           /* Command handlers (FTP commands - not used) */
     auth_mongodb_authtab,           /* Authentication handlers (getpwnam, auth) */
     auth_mongodb_init,              /* Module initialization (called at startup) */
-    auth_mongodb_sess_init          /* Session initialization (called per connection) */
+    auth_mongodb_sess_init,         /* Session initialization (called per connection) */
+    MOD_AUTH_MONGODB_VERSION        /* Module version string */
 };
